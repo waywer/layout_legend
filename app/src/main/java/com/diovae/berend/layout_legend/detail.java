44127d348889dcb4b0cd999797e260f7a9a7a9ab
berend vermeer <berend vermeer>
2017-03-10 12:41:27 +0100

    Initial commit

---
+++ app/src/main/java/com/diovae/berend/layout_legend/detail.java
@@ -0,0 +1,143 @@
+package com.diovae.berend.layout_legend;
+
+
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import java.io.IOException;
+
+
+
+public class detail extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    @SuppressWarnings("deprecation")
+    Camera camera; // camera class variable
+    SurfaceView camView; // drawing camera preview using this variable
+    SurfaceHolder surfaceHolder; // variable to hold surface for surfaceView which means display
+    boolean camCondition = false;  // conditional variable for camera preview checking and set to false
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);// get layout of detail screen(image)
+        final SeekBar seeker = (SeekBar) findViewById(R.id.seekBar);
+        // getWindow() to get window and set it's pixel format which is UNKNOWN
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        // refering the id of surfaceView
+        camView = (SurfaceView) findViewById(R.id.camerapreview);
+        final ImageView background_image = (ImageView) findViewById(R.id.imageView6);
+        // getting access to the surface of surfaceView and return it to surfaceHolder
+        surfaceHolder = camView.getHolder();
+        // adding call back to this context means MainActivity
+        surfaceHolder.addCallback(this);
+        // to set surface type
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+
+        // TODO keep the button switched if mobile is switched
+        final Switch camera_switch =(Switch) findViewById(R.id.camera_button); // when switch is pushed: make background image (in)visible
+        camera_switch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                camera_switch.setSelected(camera_switch.isPressed()); // if switch is activated: make background image invisible
+                if(camera_switch.isChecked()) {
+                    background_image.setVisibility(View.INVISIBLE);
+                }
+                else {                                               // if switch is activated: make background image visible
+                    background_image.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
+        seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) { //if the seekbar value is changed, start process
+                ImageView image = (ImageView) findViewById(R.id.detail_warImage);// image that will be change by the seekbar
+
+                float waarde = seeker.getProgress(); // create variable that has the value of the seekbar
+                image.setAlpha(waarde/255);// set alpha value of the image the same as the value of the seekbar
+            }
+
+            @Override// isn't used for the seekbar in this project but is needed
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override// isn't used for the seekbar in this project but is needed
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        // TODO Auto-generated method stub
+        // stop the camera
+        if(camCondition){
+            camera.stopPreview(); // stop preview using stopPreview() method
+            camCondition = false; // setting camera condition to false means stop
+        }
+        // condition to check whether your device have camera or not
+        if (camera != null){
+            try {
+                Camera.Parameters parameters = camera.getParameters();
+                camera.setParameters(parameters); // setting camera parameters
+                camera.setPreviewDisplay(surfaceHolder); // setting preview of camera
+                camera.startPreview();  // starting camera preview
+
+                camCondition = true; // setting camera to true which means having camera
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();//get ready to rotate
+        camera = Camera.open();   // opening camera
+        if(rotation == Surface.ROTATION_0){
+            camera.setDisplayOrientation(90);   // setting camera preview orientation to landscape
+        }
+        else if(rotation == Surface.ROTATION_90) {
+            camera.setDisplayOrientation(0);   // setting camera preview orientation to portrait
+        }
+        else if(rotation == Surface.ROTATION_270){
+            camera.setDisplayOrientation(180);   // setting camera preview orientation to reverse landscape
+        }
+    }
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        camera.stopPreview();  // stopping camera preview
+        camera.release();       // releasing camera
+        camera = null;          // setting camera to null when left
+        camCondition = false;   // setting camera condition to false also when exit from application
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig); // restart activity for the camera orientation.
+    }
+}
+
---
+++ app/src/main/java/com/diovae/berend/layout_legend/maps.java
@@ -0,0 +1,177 @@
+package com.diovae.berend.layout_legend;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+public class maps extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    protected GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googlemap) {
+        mMap = googlemap;
+
+        if (mMap != null) {
+            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    View v = getLayoutInflater().inflate(R.layout.infow_window, null);
+
+                    TextView tvHaarlem = (TextView) v.findViewById(R.id.tvHaarlem);
+                    TextView tvSnippet = (TextView) v.findViewById(R.id.tvNoord_Holland);
+
+                    LatLng ll = marker.getPosition();
+                    tvHaarlem.setText(marker.getTitle());
+                    tvSnippet.setText(marker.getSnippet());
+
+                    return v;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    return null;
+                }
+
+            });
+
+            mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                @Override
+                public void onInfoWindowClick(Marker marker) {
+                    Intent intent = new Intent(maps.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        //Add polygons
+        //Polygon Haarlem
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(52.3972493025405, 4.67563038906216), new LatLng(52.3936879470701, 4.67319886991241),
+                        new LatLng(52.3909053938453, 4.67452066841425), new LatLng(52.389737567664, 4.67771626131506),
+                        new LatLng(52.3902866618205, 4.6811903617531), new LatLng(52.3882160847386, 4.68259586820105),
+                        new LatLng(52.3874437027485, 4.68692113646379), new LatLng(52.3834728263859, 4.68600263540365),
+                        new LatLng(52.3818068716301, 4.68397248207025), new LatLng(52.3798722059021, 4.68036400206407),
+                        new LatLng(52.3774768293716, 4.67768063925706), new LatLng(52.3771906612989, 4.6774490786943),
+                        new LatLng(52.3758260857329, 4.67467575195987), new LatLng(52.3740486490056, 4.67258253901697),
+                        new LatLng(52.3715396617523, 4.67136608935146), new LatLng(52.3677415412861, 4.67224552442739),
+                        new LatLng(52.3566972045618, 4.67629936445528), new LatLng(52.355644934862, 4.67653984362748),
+                        new LatLng(52.3542871350804, 4.67644584787989), new LatLng(52.3532013412246, 4.67587556543209),
+                        new LatLng(52.351402080676, 4.67401034150446), new LatLng(52.3508621702949, 4.67308805482703),
+                        new LatLng(52.3406544061508, 4.64955165153368), new LatLng(52.3398454399224, 4.64634249088136),
+                        new LatLng(52.338906077209, 4.63964801704714), new LatLng(52.3396668843352, 4.63924322376375),
+                        new LatLng(52.3411229641406, 4.63963215360086), new LatLng(52.3421936047562, 4.64041863212025),
+                        new LatLng(52.3430211060531, 4.64059504456707), new LatLng(52.3439041406835, 4.64099903759188),
+                        new LatLng(52.3455629628869, 4.64032479968842), new LatLng(52.3476406746653, 4.64049505732217),
+                        new LatLng(52.3494703659177, 4.64031105906557), new LatLng(52.3511572689273, 4.63953560689747),
+                        new LatLng(52.3533590267873, 4.63638822563814), new LatLng(52.355320158789, 4.63514258578392),
+                        new LatLng(52.3565836693078, 4.63511792623773), new LatLng(52.358325812502, 4.63678102974035),
+                        new LatLng(52.3602175817055, 4.63737668798515), new LatLng(52.3614841764692, 4.63662633766716),
+                        new LatLng(52.3608040483072, 4.63320349990147), new LatLng(52.360724286513, 4.62990293721263),
+                        new LatLng(52.3614689731779, 4.62720638204766), new LatLng(52.3621293701589, 4.62556051429856),
+                        new LatLng(52.3614633356901, 4.61844034363014), new LatLng(52.3638996209719, 4.60573251848173),
+                        new LatLng(52.3670447141737, 4.60732388901046), new LatLng(52.3679721902126, 4.60430537296307),
+                        new LatLng(52.3713552561224, 4.60458647959545), new LatLng(52.3723772578802, 4.60151194137007),
+                        new LatLng(52.376801864733, 4.60106681926423), new LatLng(52.3774544500618, 4.59951558762392),
+                        new LatLng(52.3826096342367, 4.60050391528942), new LatLng(52.3854185374468, 4.6043487342938),
+                        new LatLng(52.3854649055612, 4.60842140530722), new LatLng(52.389821133028, 4.61081951241128),
+                        new LatLng(52.3880945409135, 4.62084926438528), new LatLng(52.3915535878494, 4.62359684299284),
+                        new LatLng(52.3955435752927, 4.62362483372662), new LatLng(52.4116658409752, 4.63164775299107),
+                        new LatLng(52.4109841290204, 4.63627942245133), new LatLng(52.4285696618644, 4.65076861048112),
+                        new LatLng(52.4259429154355, 4.66183167866398), new LatLng(52.4255616652021, 4.66515925106454),
+                        new LatLng(52.422454640913, 4.67209303228572), new LatLng(52.418722487065, 4.67468791902461),
+                        new LatLng(52.4191576497094, 4.68124186986617), new LatLng(52.4138486995183, 4.6824761689089),
+                        new LatLng(52.4096379129416, 4.67555006521692), new LatLng(52.4091422748309, 4.6723888555115),
+                        new LatLng(52.4064693165167, 4.67437943631928), new LatLng(52.4041627986238, 4.66881620991149),
+                        new LatLng(52.4013994015931, 4.66866457676291), new LatLng(52.3994774535637, 4.67868568558831),
+                        new LatLng(52.3985147343854, 4.67857027761336), new LatLng(52.3972493025405, 4.67563038906216))
+                .strokeColor(Color.GRAY)
+                .strokeWidth(2)
+                .fillColor(0x15FF0000);
+        Polygon polygon = mMap.addPolygon(rectOptions);
+
+
+        //Add Marker
+        MarkerOptions options = new MarkerOptions()
+                .title("Paviljoen Welgelegen")
+                .snippet("Manifestatie")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_mapmarker))
+                .position(new LatLng(52.3716064, 4.6285076));
+        mMap.addMarker(options);
+
+        //Set min and max zoom
+        mMap.setMinZoomPreference(12.0f);
+        mMap.setMaxZoomPreference(20.0f);
+
+
+        //Set boundaries
+        LatLngBounds Haarlem = new LatLngBounds(
+                new LatLng(52.3733, 4.6244), new LatLng(52.3982, 4.6418));
+        mMap.setLatLngBoundsForCameraTarget(Haarlem);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
